9c88bb94fcc2c416b8cd005d71f20a20f91e8846
Iury Vasconcelos <iury0393@gmail.com>
2020-04-30 23:07:34 -0300

    Alterando mais um pouco

---
+++ lib/cep_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CEP extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFEDC306),
+      appBar: AppBar(
+        title: Image.asset('assets/logo_correios.png'),
+        backgroundColor: Color(0xFFF8F3F0),
+      ),
+    );
+  }
+}
